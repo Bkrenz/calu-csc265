edd0c962310a021763da69074ebcede1e840cceb
Robert Krency <bobkrency@gmail.com>
2021-09-16 13:47:39 -0400

    Fixed input validation with regex matching, added comments and output messages.

--- Assignments/Assignment 1/main.cpp
+++ Assignments/Assignment 1/main.cpp
@@ -1,6 +1,8 @@
 /**
  * 
  * Author: Robert Krency
+ * Author: Austin Pringle
+ * Author: Anthony Stepich
  * 
  * Assignment 1 for CSC 265.
  * 
@@ -14,17 +16,25 @@
 
 using namespace std;
 
+string getValidInput();
+
 int main()
 {
+    // Initialize values
     bool continueMenu = true;
     int menuInput = 0;
 
     // Instantiate the Clock
-    // Clock clock = Clock(); // Initial value: 00:00 am
+    // Clock clock = Clock(); // Initial value: 12:00 am
 
+    // Display welcome message
+    cout << "Welcome to the Clock command line interface.\n\n";
+
+    // While the system should continue running
     while(continueMenu)
     {
         // Display the Menu
+        cout << endl;
         cout << "1. Set the Time" << endl;
         cout << "2. Display the Time" << endl;
         cout << "3. Advance one hour" << endl;
@@ -32,9 +42,10 @@ int main()
         cout << "5. Quit" << endl;
 
         // Get the Input
+        cout << "Please make a selection from the menu: ";
         while(!(cin >> menuInput) || cin.peek() != '\n' && menuInput > 0 && menuInput < 13)
         {
-            cout << "Please enter an INTEGER: ";
+            cout << "Please make a selection from the menu: ";
             cin.clear();
             cin.ignore(30000, '\n');            
         }
@@ -42,19 +53,19 @@ int main()
         // Process Input
         switch(menuInput) 
         {
-            case 1:
-                // clock.setTime(getValidInput());
+            case 1: // Set the time
+                // clock.setTime(getValidInputTime());
                 break;
 
-            case 2:
+            case 2: // Display the time
                 // clock.displayTime();
                 break;
 
-            case 3:
+            case 3: // Increment the hour
                 // clock.incrementHour();
                 break;
             
-            case 4: 
+            case 4: // Increment the minute
                 // clock.incrementMinute();
                 break;
 
@@ -63,31 +74,64 @@ int main()
                 break;
 
             default: // Error in input
-                cout << "Error; try again.";
+                cout << "Error; try again." << endl;
         }
     }
+
+    // Quit the program
+    cout << "Thank you, come again!";
+    return 0;
 
 }
 
 /**
+ * validateInputTime()
+ * 
+ * Checks if the input string matches a valid representation of a clock's time, in the format:
+ *      hh:mm aa
+ * 
+ * Precondition:
+ *      (string) input - a string representing an input value from cin
+ * Returns:
+ *      A boolean value of whether the string is a valid input for a clock 
+ * 
+ */
+bool validateInputTime(string  input){
+
+    // A regex string that matches to 12:00 time with mandatory meridians 
+    // - from https://stackoverflow.com/questions/7536755/regular-expression-for-matching-hhmm-time-format/7536768
+    string TIME_REGEX = "((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))";
+
+    return regex_match(input, regex(TIME_REGEX));
+}
+
+/**
  * getValidInput()
+ * 
+ * Gets an input string from the user for the time for the clock
+ * 
  * Precondition: None
  * Returns:
  *      (string) input - an input time in the format "hh:mm am/pm" 
  */
-string getValidInput(){
+string getValidInputTime(){
     string input;
 
-    // A regex string that matches to 12:00 time with mandatory meridians 
-    // - from https://stackoverflow.com/questions/7536755/regular-expression-for-matching-hhmm-time-format/7536768
-    string TIME_REGEX = "/((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/";
+    // Clean up cin
+    cin.clear();
+    cin.ignore(30000, '\n');
+
+    // Get the first input
+    cout << "Input a valid time in format hh:mm aa: " << endl;
+    getline(cin, input);
     
-    while(true)
+    // Loop until there is a valid input.
+    while(!validateInputTime(input))
     {
-        getline(cin, input);
-        if (regex_match(input, regex(TIME_REGEX)))
-            return input;
-        else
-            cout << "Invalid input, try again." << endl;
+        cout << "Invalid input, try again." << endl;
+        getline(cin, input);    
     }
+
+    // Return the valid input
+    return input;
 }
