0066e28ef592c506d831c337e1280f7cf0d60173
Robert Krency <bobkrency@gmail.com>
2021-10-13 17:37:11 -0400

    comments, assignment 2 moved to new repo

--- Assignments/Assignment 2/list.h
+++ Assignments/Assignment 2/list.h
@@ -11,22 +11,48 @@
 #ifndef __LIST_H__
 #define __LIST_H__
 
+/**
+ *  Class List
+ *      Defines a list with appropriate functions for removing and inserting elements, and printing the list
+ *  
+ *  void insert(const int data)
+ *      description: Inserts a value at the end of the list
+ *      precondition: none
+ *      postcondition: The data input is added to the end of the list
+ * 
+ *  void remove(const int data)
+ *      description: Removes the first occurrence of the input data, if it exists, and shifts all data in the list to the left
+ *      precondition: none
+ *      postcondition: Data removed if exists
+ * 
+ *  void printList() const 
+ *      description: Prints the list, with each element on a new line
+ *      precondition: none
+ *      postcondition: List printed to output
+ * 
+ */
+
 class List {
 
     public:
         List();
         ~List();
+
+        void printList() const;
+        
+        /* Modification Methods */
         void insert(const int data);
         void remove(const int data);
-        void printList() const;
+        
+        /* Constant Accessor Methods */
         int getLength() const;
 
     private:
-        int arrayLength;
-        int arraySize;
+        int arrayLength;    // Tracks how many items are in the array
+        int arraySize;      // Tracks the total size of the array
 
     protected:
-        int* array;
+        int* array;         // Dynamic array as the list
 
 };
 
